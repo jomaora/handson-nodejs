57197ade9a09fa51f3388a6f0304c277c0f0d5d5
Joan Ortega <jomaora@gmail.com>
2015-05-25 16:23:59 +0200

    Adding lodash and promises lib. Script to set favorites arrays on users

--- mongoose/users.js
+++ mongoose/users.js
@@ -5,7 +5,10 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
     login: String,
     name: String,
-    favorites: [String]
+    favorites: [{
+    	_id:  {type: mongoose.Schema.Types.ObjectId},
+    	title: String
+    }]
 });
 
 module.exports = mongoose.model('user', userSchema);
